fcf0853eda9bf167d3fdc6cbc853c56ac63036cf
ktw9807 <ktw9807@naver.com>
2023-05-08 14:40:11 +0900

    Feat: Chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,316 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import axios from 'axios';
+import { useState } from 'react';
+import { IoSendSharp } from "react-icons/io5";
+import { FiSearch } from "react-icons/fi";
+import { FiChevronLeft } from "react-icons/fi";  
+import ChatInfo from './ChatInfo';
+
+const sizes = {
+  tablet: 1024,
+  phone: 768,
+};
+
+// 자동으로 media 쿼리 함수를 만들어 준다.
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
+{/* 메인 */}
+const MainWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+{/* 챗리스트 Section 전체 */}
+const ListSectionWrap = styled.section`
+  width: 30vw;
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+`
+
+{/* 채팅 Section 전체 */}
+const ChatSectionWrap = styled.section`
+  width: 70vw;
+  height: 100vh;
+  justify-content: center;
+`
+
+{/* 챗리스트 DIV */}
+const ChatListWrap = styled.section`
+  margin: 3rem auto;
+  width: 90%;
+  height: 80%;
+  justify-content: center;
+  background-color: black;
+  border-radius: 5rem;
+  background: #FFFFFF;
+  box-shadow: 0rem 0.1rem 0.5rem 0.1rem rgba(0, 0, 0, 0.1);
+
+`
+
+{/* 채팅 DIV */}
+const ChatGptWrap = styled.div`
+  justify-content: center;
+  margin: 3rem auto;
+  width: 90%;
+  height: 80%;
+  background-color: black;
+  border-radius: 5rem;
+  background: #FFFFFF;
+  box-shadow: 0rem 0.1rem 0.5rem 0.1rem rgba(0, 0, 0, 0.1);
+
+`
+
+{/* 챗리스트 상단 DIV */}
+const ChatListTop = styled.div`
+  display: flex;
+  width: 100%;
+  height: 5rem;
+  margin-top: 2rem;
+
+  h1{
+
+    position: absolute;
+    font-style: normal;
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    margin: 1rem 2.5rem;
+    font-weight: 600;
+    color: #6AC7B2;
+  }
+
+`
+
+{/* 챗리스트 중단 DIV */}
+const ChatListMiddle = styled.div`
+  display: flex;
+  width: 100%;
+  height: 4rem;
+  justify-content: center;
+
+`
+{/* 챗리스트 검색창 */}
+const ChatText = styled.input`
+  position: absolute;
+  height: 5%;
+  width: 23%;
+  border-radius: 1rem;
+  border-style: hidden;
+  background-color: rgba(217, 217, 217, 0.4);
+  font-size: 1.5rem;
+  text-indent: 2rem;
+
+`
+{/* 챗리스트 하단 DIV */}
+const ChatListBottom = styled.div`
+  width: 90%;
+  height: 27rem;
+  justify-content: center;
+  margin: 1.5rem auto;
+`
+
+{/* 챗리스트 List */}
+const RecordList = styled.li`
+  display: flex;
+  width: 90%;
+  height: 4rem;
+  margin: 1rem auto;
+
+  h1{
+    width: 100%;
+    margin: 0.4rem auto;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5rem;
+    border-bottom: 0.1rem solid rgba(130, 130, 130, 0.4);
+  }
+
+  h2{
+    width: 40%;
+    margin: 0.4rem auto;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 1rem;
+    border-bottom: 0.1rem solid rgba(130, 130, 130, 0.4);
+  }
+
+  button{
+    
+  }
+`
+{/* 채팅 상단 DIV */}
+const ChatGptTop = styled.div`
+  display: flex;                       
+  width: 100%;
+  height: 30%;
+  border-radius: 5rem;
+  background: rgba(224, 224, 224, 0.4);
+  max-height: 20rem;
+
+  h1{
+    width: 100%;
+    margin: 3rem auto;
+    margin-left: 4rem;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5rem;
+    color: black
+  }
+`
+
+{/* 채팅 상단 Button */}
+const ChatResume = styled.button`
+  border: none;
+  margin-top: 1.5rem;
+  background: rgba(240, 240, 240, 0.4);
+  margin-left: 2rem;
+  cursor: pointer;
+  width: 0rem;
+  height: 0rem;
+`
+
+{/* 채팅 중단 DIV */}
+const ChatGptMiddle = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60%;
+  background: white;
+  margin-top: -5rem;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: rgba(127, 168, 157, 0.3); /* 스크롤바 색상 */
+    border-radius: 5rem; /* 스크롤바 둥근 테두리 */
+  }
+  ::-webkit-scrollbar-track {
+    background: white;  /*스크롤바 뒷 배경 색상*/
+  }
+  h1{
+    width: 100%;
+    margin: 3.5rem auto;
+    margin-left: 4rem;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5rem;
+    color: black
+  }
+`
+
+{/* 채팅 하단 DIV */}
+const ChatGptBottom = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10%;
+  justify-content: center;
+  background: white;
+  border-radius: 5rem;
+  margin-top: 2rem;
+
+`
+
+{/* 채팅 입력창 */}
+const ChatInput = styled.input`
+  position: absolute;
+  height: 7%;
+  width: 50%;
+  border-radius: 3rem;
+  border-style: hidden;
+  background-color: rgba(127, 168, 157, 0.3);
+  font-size: 1.5rem;
+  text-indent: 2rem;
+
+
+`
 
 const Chat = () => {
-  return <div>챗봇</div>;
+
+  const [Subject, setSubject] = useState();
+  const [isSelect, setisOn] = useState(false);
+
+  // List 클릭 시 토글
+  const ListClick = (name, e) => {
+    setSubject(name);
+    setisOn(!isSelect);
+  };
+
+  return (
+    <MainWrap>
+      <ListSectionWrap>
+        <ChatListWrap>
+          
+          <ChatListTop>
+            <h1>Chat List</h1>
+          </ChatListTop>
+          
+          <ChatListMiddle>
+            <ChatText placeholder='Search'/>
+          </ChatListMiddle>
+
+          <ChatListBottom>
+
+            <RecordList onClick={(e) => {ListClick("Business", e)}}>
+              <h1>Business</h1>
+              <h2>6:56 PM</h2>
+            </RecordList>
+            <RecordList onClick={(e) => {ListClick("Daily Life", e)}}>
+              <h1>Daily Life</h1>
+              <h2>Yesterday</h2>
+            </RecordList>
+            <RecordList onClick={(e) => {ListClick("Education", e)}}>
+              <h1>Education</h1>
+              <h2>Yesterday</h2>
+            </RecordList>
+            <RecordList onClick={(e) => {ListClick("Interest", e)}}>
+              <h1>Interest</h1>
+              <h2>Tuesday</h2>
+            </RecordList>
+
+          </ChatListBottom>
+
+        </ChatListWrap>
+      </ListSectionWrap>
+
+      <ChatSectionWrap>
+
+        <ChatGptWrap>
+
+          <ChatGptTop>
+            <ChatResume onClick={(e) => {ListClick(null, e)}}>
+              <FiChevronLeft size="4rem" color="#6AC7B2"></FiChevronLeft>
+            </ChatResume>
+            <h1>{Subject}</h1>
+          </ChatGptTop>
+
+          <ChatGptMiddle>
+
+            {/* Subject가 null일경우 출력*/}
+            {!Subject && (<ChatInfo> </ChatInfo>)}
+
+          </ChatGptMiddle>
+
+          <ChatGptBottom>
+            <ChatInput placeholder='Type your message here'></ChatInput>
+          </ChatGptBottom>
+
+        </ChatGptWrap>
+
+      </ChatSectionWrap>
+      
+    </MainWrap>
+  );
 };
 
 export default Chat;
---
+++ src/components/chat/ChatInfo.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import correctLogo from '../../assets/correct-logo.png';
+
+const sizes = {
+    tablet: 1024,
+    phone: 768,
+  };
+
+// 자동으로 media 쿼리 함수를 만들어 준다.
+const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+      @media (max-width: ${sizes[label] / 16}em) {
+        ${css(...args)};
+      }
+    `;
+    return acc;
+  }, {});
+
+const ChatInfo = () => {
+
+    const InfoWrap = styled.div`
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        justify-content: center;
+
+    `
+
+    const ImageWrap = styled.div`
+        display: flex;
+
+        img{
+            margin: 3rem auto;
+            width: 10rem;
+            height: 10rem;
+        }
+    `
+
+    const TextWrap = styled.div`
+        display: flex;
+        width: 100%;
+        height: 25%;
+
+        h1{
+            margin: 3rem auto;
+            width: 30rem;
+            height: 10rem;
+            font-style: normal;
+            font-weight: 500;
+            font-size: 1.5rem;
+
+        }
+
+        h2{
+            margin: 1rem auto;
+            width: 30rem;
+            height: 10rem;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 1rem;
+        }
+    `
+
+    return (
+        <InfoWrap>
+            <ImageWrap>
+                <img src={correctLogo} alt="correcting 로고" />
+            </ImageWrap>
+            <TextWrap>
+                <h1>Correct-ing과 함께 공부해 보세요</h1>
+            </TextWrap>
+            <TextWrap>
+                <h2>주제를 선택해 주세요</h2>
+            </TextWrap>
+        </InfoWrap>
+    )
+}
+export default ChatInfo;
