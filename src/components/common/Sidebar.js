38bc4b26d99a7a0df89ee5fd14ee275ddbf00360
yeriniii <yerini000303@naver.com>
2023-05-17 17:01:01 +0900

    Fix: 사이드바 다시 추가

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import correctLogo from '../../assets/correct-logo.png';
@@ -7,12 +7,13 @@ import { IoGameController } from 'react-icons/io5';
 import { IoIosClose } from 'react-icons/io';
 import { BsFillClipboard2CheckFill } from 'react-icons/bs';
 import { BsChatLeftTextFill } from 'react-icons/bs';
+import { ImEnter } from 'react-icons/im';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from '../../../node_modules/react-redux/es/exports';
 const sizes = {
   tablet: 1024,
   phone: 768,
 };
-
 // 자동으로 media 쿼리 함수를 만들어 준다.
 const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
@@ -24,23 +25,20 @@ const media = Object.keys(sizes).reduce((acc, label) => {
 }, {});
 
 const MainWrap = styled.div`
-  @media screen and (min-width: 1025px) {
-    display: none;
-  }
   position: fixed;
   display: flex;
   flex-direction: column;
+  justify-content:none;
   align-items: center;
   top: 0;
   right: 0;
-  width: 45vw;
-  height: 100vh;
+  width: 45%;
+  height: 100%;
   background-color: white;
   padding: 1rem;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease-in-out;
   transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(100%)')};
-  z-index: 99;
 `;
 
 const LogoWrap = styled.div`
@@ -84,7 +82,7 @@ const SelectWrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 0.5rem;
+  margin-top: 1.2rem;
   gap: 1.5rem;
 
   button {
@@ -92,13 +90,10 @@ const SelectWrap = styled.div`
       color: red;
     }
   }
-  border-bottom: 1px solid #142231;
 `;
-
 const StyledButton = styled.button`
   width: 90%;
   display: flex;
-  //justify-content: center;
   align-items: center;
   gap: 1rem;
   background-color: white;
@@ -118,14 +113,18 @@ const StyledButton = styled.button`
   &:hover {
     background-color: #e0f2f6;
   }
-
-  svg {
-    margin: 0;
-  }
 `;
-const Sidebar = ({ nickname, isOpen, onClose, onLogout }) => {
+const Sidebar = ({ nickname, isOpen: initialIsOpen, onClose, onLogout }) => {
+  const { loginRes } = useSelector(({ auth }) => ({
+    loginRes: auth.loginRes,
+  }));
+
   const sidebarRef = useRef(null);
   const location = useLocation();
+  /*초기값을 'initialIsOpen' prop로 전달된 값으로 설정*/
+  const [isOpen, setIsOpen] = useState(initialIsOpen);
+
+  /* 밖 클릭시 닫히는 코드 */
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
@@ -139,8 +138,28 @@ const Sidebar = ({ nickname, isOpen, onClose, onLogout }) => {
     };
   }, [onClose]);
 
+/* 화면 크기가 변경될 때마다 isOpen 상태를 업데이트. 너비가 1025px 이상이면 isOpen 상태를 false로 변경 */
+    useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1025) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  /* 'initialIsOpen' prop 값이 변경될 때마다 isOpen 상태업뎃 */
+  useEffect(() => {
+    setIsOpen(initialIsOpen);
+  }, [initialIsOpen]);
+
   return (
     <MainWrap ref={sidebarRef} isOpen={isOpen}>
+
       <LogoWrap>
         <div>
           <img src={correctLogo} alt="correcting 로고" />
@@ -171,12 +190,24 @@ const Sidebar = ({ nickname, isOpen, onClose, onLogout }) => {
             게임하기
           </Link>
         </StyledButton>
-        <StyledButton>
+        
+        {nickname ? (
+       
+          <StyledButton>
           <ImExit size="25" />
           <Link to="#" onClick={onLogout}>
             로그아웃
           </Link>
-        </StyledButton>
+          </StyledButton>
+        ): (
+          <StyledButton>
+            <ImEnter size="25"/>
+            <Link to="/login" onClick={onClose}>
+              로그인
+            </Link>
+          </StyledButton>
+        
+        )}
       </SelectWrap>
     </MainWrap>
   );
