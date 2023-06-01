aad1e7c70d350030103ede9c4573f4d687df4367
minseoJeong37@gmail.com <wjdalstj330@gmail.com>
2023-06-02 00:43:32 +0900

    Style: 게임 디자인 수정

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,73 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 // 50문제 랜덤
 const words = [
-  "the quick brown fox jumps",
-  "life is short make it",
-  "keep calm and code on",
-  "practice makes perfect",
-  "dream big work hard",
-  "stay focused and never give up",
-  "be yourself everyone else is taken",
-  "success is the sum of small efforts",
-  "believe in yourself and all that you are",
-  "do what you love love what you do",
-  "i love walking dogs",
-  "coffee fuels my mornings",
-  "reading books relaxes me",
-  "music enhances my cooking",
-  "traveling brings me joy",
-  "running in parks refreshes me",
-  "loved ones make me happy",
-  "painting sparks my creativity",
-  "journaling clears my mind",
-  "yoga keeps me balanced",
+  'the quick brown fox jumps',
+  'life is short make it',
+  'keep calm and code on',
+  'practice makes perfect',
+  'dream big work hard',
+  'stay focused and never give up',
+  'be yourself everyone else is taken',
+  'success is the sum of small efforts',
+  'believe in yourself and all that you are',
+  'do what you love love what you do',
+  'i love walking dogs',
+  'coffee fuels my mornings',
+  'reading books relaxes me',
+  'music enhances my cooking',
+  'traveling brings me joy',
+  'running in parks refreshes me',
+  'loved ones make me happy',
+  'painting sparks my creativity',
+  'journaling clears my mind',
+  'yoga keeps me balanced',
   "rain's sound soothes me",
-  "board games with friends are fun",
-  "nature inspires me",
-  "trying new recipes is an adventure",
-  "dance expresses my freedom",
-  "gardening is therapeutic",
-  "movies help me unwind",
-  "puzzles challenge and entertain me",
-  "photography captures moments",
-  "helping others fulfills me",
-  "learning is a lifelong journey",
-  "laughter is the best medicine",
-  "friends are the family we choose",
-  "creativity knows no bounds",
-  "adventure awaits at every corner",
-  "kindness costs nothing",
-  "hard work pays off",
-  "imagination is the key to innovation",
-  "failure is a stepping stone to success",
-  "patience is a virtue",
-  "simplicity is the ultimate sophistication",
-  "knowledge is power",
-  "time is precious, use it wisely",
-  "love conquers all",
-  "actions speak louder than words",
-  "change starts from within",
-  "happiness is a choice",
-  "beauty is in the eye of the beholder",
-  "forgiveness sets you free",
-  "hope is a powerful motivator",
-  "self-care is important for well-being",
-  "gratitude turns what we have into enough",
-  "embrace the unknown",
-  "positivity breeds positivity",
-  "success comes to those who persevere",
-  "inspiration can be found everywhere",
-  "learning from mistakes leads to growth",
-  "a journey of a thousand miles begins with a single step",
-  "challenges make you stronger",
-  "kindness is contagious",
-  "stay curious, keep learning",
-  "dreams have no limits",
-  "be the change you wish to see in the world",
+  'board games with friends are fun',
+  'nature inspires me',
+  'trying new recipes is an adventure',
+  'dance expresses my freedom',
+  'gardening is therapeutic',
+  'movies help me unwind',
+  'puzzles challenge and entertain me',
+  'photography captures moments',
+  'helping others fulfills me',
+  'learning is a lifelong journey',
+  'laughter is the best medicine',
+  'friends are the family we choose',
+  'creativity knows no bounds',
+  'adventure awaits at every corner',
+  'kindness costs nothing',
+  'hard work pays off',
+  'imagination is the key to innovation',
+  'failure is a stepping stone to success',
+  'patience is a virtue',
+  'simplicity is the ultimate sophistication',
+  'knowledge is power',
+  'time is precious, use it wisely',
+  'love conquers all',
+  'actions speak louder than words',
+  'change starts from within',
+  'happiness is a choice',
+  'beauty is in the eye of the beholder',
+  'forgiveness sets you free',
+  'hope is a powerful motivator',
+  'self-care is important for well-being',
+  'gratitude turns what we have into enough',
+  'embrace the unknown',
+  'positivity breeds positivity',
+  'success comes to those who persevere',
+  'inspiration can be found everywhere',
+  'learning from mistakes leads to growth',
+  'a journey of a thousand miles begins with a single step',
+  'challenges make you stronger',
+  'kindness is contagious',
+  'stay curious, keep learning',
+  'dreams have no limits',
+  'be the change you wish to see in the world',
 ];
 
 const MainDiv = styled.div`
@@ -87,10 +87,10 @@ const RankDiv = styled.div`
   align-items: center;
   border-radius: 5rem;
   border-radius: 50px;
-  border: 1px solid #D9D9D9;
+  border: 1px solid #d9d9d9;
   background: #ffffff;
 
-  h1{
+  h1 {
     margin: 2rem auto;
   }
 `;
@@ -119,7 +119,7 @@ const TimeBarDiv = styled.div`
 const TextInput = styled.input`
   font-size: 2rem;
   width: 50%;
-  margin: 4rem; auto;
+  margin: 4rem auto;
   padding: 1rem;
   border-radius: 1rem;
   border: 1px solid #ccc;
@@ -127,58 +127,65 @@ const TextInput = styled.input`
 `;
 
 const ProblemWrap = styled.div`
-    width: 85%;
-    height:80%;
-    background-color:#FFFFFF;
-    border-radius: 50px;
-    border: 1px solid #D9D9D9;
-    align-items: center;
-    text-align: center;
-    margin-top: 1rem;
-    p{
-      line-height: 2em;
-    }
-    h1{
-      font-size: 3rem;
-      margin: 5rem auto;
-    }
-    button {
-      margin: 5rem auto;
-      width: 8rem;
-      height: 4rem;
-      border: none;
-      border-radius: 1rem;
-      background-color: #ccc;
-      transition: background-color 0.3s, cursor 0.3s;
-      cursor: pointer;
-    }
-    
-    button:hover {
-      background-color: #aaa;
-    }
-    
-    button:active {
-      background-color: #888;
-      cursor: grabbing;
-    }
+  width: 85%;
+  height: 80%;
+  background-color: #ffffff;
+  border-radius: 50px;
+  border: 1px solid #d9d9d9;
+  text-align: center;
+  margin-top: 1rem;
+  p {
+    line-height: 2em;
+  }
+  h1 {
+    font-size: 2rem;
+    margin: 5rem auto;
+    font-weight: 700;
+  }
+  button {
+    margin: 5rem auto;
+    width: 8rem;
+    height: 4rem;
+    border: none;
+    border-radius: 1rem;
+    background-color: #ccc;
+    transition: background-color 0.3s, cursor 0.3s;
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 1.3rem;
+    padding: 0.4rem 0.8rem;
+    background-color: #eaf5f3;
+  }
+
+  button:hover {
+    background-color: #6ac7b2;
+  }
+
+  button:active {
+    background-color: #888;
+    cursor: grabbing;
+  }
 `;
 const AnswerWrap = styled.div`
-    width: 100%;
-    height:50%;
-    align-items: center;
-    text-align: center;
-    background-color:#EAF5F3;
-    border-radius: 50px 50px 0px 0px;
+  width: 100%;
+  height: 50%;
+  align-items: center;
+  text-align: center;
+  background-color: #eaf5f3;
+  border-radius: 50px 50px 0px 0px;
+  display: flex;
+  justify-content: center;
 
-    p{
-      line-height: 8em;
-      font-size: 2rem;
-    }
+  p {
+    line-height: 8em;
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
 `;
 
 const Game = () => {
-  const [currentWord, setCurrentWord] = useState("");
-  const [userInput, setUserInput] = useState("");
+  const [currentWord, setCurrentWord] = useState('');
+  const [userInput, setUserInput] = useState('');
   const [score, setScore] = useState(0);
   const [timeLeft, setTimeLeft] = useState(1000000);
   const [gameOver, setGameOver] = useState(false);
@@ -186,7 +193,7 @@ const Game = () => {
   const [gameStart, setGameStart] = useState(false);
   const [rankData, setRankData] = useState([]);
   const [buttonVisible, setButtonVisible] = useState(true);
-  const [count, setCount] = useState("");
+  const [count, setCount] = useState('');
   const { loginRes } = useSelector(({ auth }) => ({
     form: auth.login, // 상태 값 설정
     loginRes: auth.loginRes,
@@ -198,11 +205,11 @@ const Game = () => {
     if (timeLeft > -1) {
       const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
       return () => clearTimeout(timer);
-    } else if (timeLeft < 0){
+    } else if (timeLeft < 0) {
       setTimeLeft(0);
       setGameOver(true);
     }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [timeLeft]);
 
   const handleInput = (event) => {
@@ -213,7 +220,7 @@ const Game = () => {
     setTimeLeft((prevTimeLeft) => prevTimeLeft - 0.3);
   };
 
-  const startGame = () =>{
+  const startGame = () => {
     let countdown = 3;
     const countdownInterval = setInterval(() => {
       setCount(countdown);
@@ -225,21 +232,20 @@ const Game = () => {
         setTimeLeft(15);
       }
     }, 1000);
-    
-  }
+  };
 
   const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       const input = userInput.trim();
       if (input === currentWord) {
         setScore(score + 10);
         const nextWord = words[Math.floor(Math.random() * words.length)];
         setCurrentWord(nextWord);
-        setUserInput("");
+        setUserInput('');
         setTimeLeft(15);
         const multiplier = score >= 1 ? 2 : 1; // 첫 번째 맞춤일 때는 1, 그 이후로는 2를 곱하여 가중치 부여
         decreaseTime(multiplier);
-      } else{
+      } else {
         // 틀렸을때
         setInCorrect(true);
         setTimeout(() => {
@@ -247,7 +253,7 @@ const Game = () => {
           setInCorrect(false);
         }, 2000);
       }
-      setUserInput("");
+      setUserInput('');
     }
   };
 
@@ -257,62 +263,61 @@ const Game = () => {
 
   useEffect(() => {
     if (score > 0) {
-      const updatedTimeLeft = 15 - (score * 0.3 / 10);
+      const updatedTimeLeft = 15 - (score * 0.3) / 10;
       setTimeLeft(updatedTimeLeft);
     }
   }, [score]);
 
   const getBarColor = (timeLeft) => {
     if (timeLeft <= 2) {
-      return "red";
+      return 'red';
     } else if (timeLeft <= 4) {
-      return "yellow";
+      return 'yellow';
     } else {
-      return "green";
+      return 'green';
     }
   };
 
   const barStyle = {
     width: `${(timeLeft / 60) * 300}%`,
-    transition: "width 1s linear",
+    transition: 'width 1s linear',
     backgroundColor: getBarColor(timeLeft),
   };
 
-  const saveScore = async () =>{
-    const url = 'http://correcting-env.eba-harr53pi.ap-northeast-2.elasticbeanstalk.com/api/v1/games';
-    
+  const saveScore = async () => {
+    const url =
+      'http://correcting-env.eba-harr53pi.ap-northeast-2.elasticbeanstalk.com/api/v1/games';
+
     try {
-      const response = await axios.post(
-        url,
-        null,
-        {
-          params: {
-            score,
-          },
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
+      const response = await axios.post(url, null, {
+        params: {
+          score,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
       // Handle response if needed
       console.log(response.data);
-
     } catch (error) {
       // Handle error if needed
       console.error(error);
     }
-    alert("점수가 기록되었습니다!");
+    alert('점수가 기록되었습니다!');
     setButtonVisible(false);
-  }
+  };
 
-  const getRank = async () =>{
+  const getRank = async () => {
     try {
       const headers = {
         Authorization: `Bearer ${accessToken}`,
       };
 
-      const response = await axios.get('http://correcting-env.eba-harr53pi.ap-northeast-2.elasticbeanstalk.com/api/v1/games', { headers });
+      const response = await axios.get(
+        'http://correcting-env.eba-harr53pi.ap-northeast-2.elasticbeanstalk.com/api/v1/games',
+        { headers },
+      );
       const data = response.data;
 
       setRankData(data); // 데이터를 상태로 업데이트합니다.
@@ -320,118 +325,110 @@ const Game = () => {
     } catch (error) {
       console.error('Error fetching games:', error);
     }
-  }
+  };
 
   useEffect(() => {
     getRank();
-            // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // 컴포넌트가 마운트되었을 때 한 번만 데이터를 가져오기 위해 useEffect 사용
-
 
   if (gameOver) {
     return (
       <MainDiv>
         <RankDiv>
-
           <h1>Top 5</h1>
           {rankData.map((item, index) => {
             if (index < 5) {
               return (
-                <h1 key={index}> {index + 1}위: {item.name} - {item.score}Point</h1>
+                <h1 key={index}>
+                  {' '}
+                  {index + 1}위: {item.name} - {item.score}Point
+                </h1>
               );
             } else {
               return null; // 4 이후에는 아무것도 렌더링하지 않음
             }
           })}
-
         </RankDiv>
         <GameDiv>
           <ProblemWrap>
             <h1>게임 오버</h1>
-            <br/>
+            <br />
             <h1>최종 점수: {score}</h1>
-            {buttonVisible && (
-              <button onClick={saveScore}>점수 저장</button>
-            )}
+            {buttonVisible && <button onClick={saveScore}>점수 저장</button>}
           </ProblemWrap>
         </GameDiv>
       </MainDiv>
-      
     );
   }
 
-  if (!gameStart){
-    return(
+  if (!gameStart) {
+    return (
       <MainDiv>
         <RankDiv>
-
           <h1>Top 5</h1>
           {rankData.map((item, index) => {
             if (index < 5) {
               return (
-                <h1 key={index}> {index + 1}위: {item.name} - {item.score}Point</h1>
+                <h1 key={index}>
+                  {' '}
+                  {index + 1}위: {item.name} - {item.score}Point
+                </h1>
               );
             } else {
               return null; // 4 이후에는 아무것도 렌더링하지 않음
             }
           })}
-       
         </RankDiv>
         <GameDiv>
           <h1>{count}</h1>
           <ProblemWrap>
-
             <AnswerWrap>
               <p>이 곳에 나타나는 문장을 똑같이 타이핑 해주세요!</p>
             </AnswerWrap>
 
             <button onClick={startGame}>시작하기</button>
-
           </ProblemWrap>
-
         </GameDiv>
       </MainDiv>
-    )
+    );
   } else {
-
     return (
       <MainDiv>
         <RankDiv>
-
           <h1>Top 5</h1>
           {rankData.map((item, index) => {
             if (index < 5) {
               return (
-                <h1 key={index}> {index + 1}위: {item.name} - {item.score}Point</h1>
+                <h1 key={index}>
+                  {' '}
+                  {index + 1}위: {item.name} - {item.score}Point
+                </h1>
               );
             } else {
               return null; // 4 이후에는 아무것도 렌더링하지 않음
             }
           })}
-
         </RankDiv>
         <GameDiv>
-          <TimeBarDiv style={barStyle}/>
+          <TimeBarDiv style={barStyle} />
           <ProblemWrap>
             <AnswerWrap>
               <p>{currentWord}</p>
             </AnswerWrap>
-              <TextInput
-                type="text"
-                value={userInput}
-                onChange={handleInput}
-                onKeyPress={handleKeyPress}
-              />
+            <TextInput
+              type="text"
+              value={userInput}
+              onChange={handleInput}
+              onKeyPress={handleKeyPress}
+            />
             <p>남은 시간: {timeLeft.toFixed(1)}초</p>
             <p>점수: {score}</p>
-            {inCorrect && (<p style={{ color: 'red' }}>틀렸습니다!</p>)}
+            {inCorrect && <p style={{ color: 'red' }}>틀렸습니다!</p>}
           </ProblemWrap>
-
         </GameDiv>
-
       </MainDiv>
     );
-  };
-}
+  }
+};
 export default Game;
-
